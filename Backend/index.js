1af366c3ccfcfae4db910375480846d518a193a1
siddharth goyal <113788213+siddharthgoyal00@users.noreply.github.com>
2024-09-01 23:43:46 +0530

    Backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,14 @@
 import {express} from "express";
+import {cors} from "cors";
+app.use(cors());
+app.use(express.json());
 import {mainRouter} from "./routes/index.js";
+
 const app = express();
 
-app.use("api/v1", mainRouter );   // used to handle the middlewares
+app.use("/api/v1", mainRouter );   // used to handle the middlewares
+
+
+
+
+app.listen(3000);
