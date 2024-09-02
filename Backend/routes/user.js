9022974fa560a6c8e8af4bc847857bbcd7deea50
siddharth goyal <113788213+siddharthgoyal00@users.noreply.github.com>
2024-09-02 18:33:26 +0530

    backend

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,5 +1,49 @@
 import {express} from "express";
-
+import {zod} from "zod";
+import {User} from "../db";
+import {JWT} from "jsonwebtoken";
 const router =  express.Router();
 
+const signUpSchema = zod.object({
+    username:  zod.string().email(),
+    password:  zod.string(),
+    firstName: zod.string(),
+    lastName:  zod.string()
+})
+router.post("/signup", async (req,res)=>{
+  const body = req.body();
+  const {success} = signUpSchema.safeParse(req.body);
+  if (!success){
+    return res.json({
+        msg: "wrong inputs / Email already taken"
+    })
+  }
+
+  const user  = User.findOne({
+       username :  body.username 
+ })
+
+ if(user._id){
+    return res.json({
+        msg: "wrong inputs / Email already taken"
+    })
+ }
+
+
+
+});
+
+
+
+router.post("/signin", );
+
+
+
+
+
+
+
+
 module.exports = router;
+
+
