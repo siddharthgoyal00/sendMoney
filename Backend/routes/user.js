ff8d66b5399c0f91e2b46f82767b9d64c8e491f5
siddharth goyal <113788213+siddharthgoyal00@users.noreply.github.com>
2024-09-06 09:57:30 +0530

    user-routes

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,15 +1,18 @@
 import {express} from "express";
 import {zod} from "zod";
 import {User} from "../db";
-import {JWT} from "jsonwebtoken";
+import {JWT_SECRET} from "jsonwebtoken";
 const router =  express.Router();
 
+//======================== zod schema for the incomming user =========================
 const signUpSchema = zod.object({
     username:  zod.string().email(),
     password:  zod.string(),
     firstName: zod.string(),
     lastName:  zod.string()
 })
+
+// make sure the schema is valid 
 router.post("/signup", async (req,res)=>{
   const body = req.body();
   const {success} = signUpSchema.safeParse(req.body);
@@ -19,6 +22,7 @@ router.post("/signup", async (req,res)=>{
     })
   }
 
+  // check weather the user exist or not 
   const user  = User.findOne({
        username :  body.username 
  })
@@ -29,20 +33,31 @@ router.post("/signup", async (req,res)=>{
     })
  }
 
-
-
+ const dbUser = await User.create (body);
+ const token = jwt.sign({
+  userId : dbUser._id
+ }, JWT_SECRET)
+res.json({
+  msg: "user created successfully",
+  token: token 
+})
+ 
 });
 
+const signInSchema = zod.objects({
+  username: zod.string().email(),
+  password: zod.string()
+})
 
-
-router.post("/signin", );
-
-
-
-
-
-
-
+router.post("/signin", async(req,res)=>{
+  const {success} = signInSchema.safeParse(req.body);
+  if(!success){
+      return res.json({
+        msg:"input incorrect"
+      })
+  }
+  
+});
 
 module.exports = router;
 
